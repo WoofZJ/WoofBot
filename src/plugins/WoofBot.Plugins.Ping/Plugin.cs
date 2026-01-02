86d281842612858ba0ddcfccfa3d6819e65d4c76
WoofZJ <zjwoof@gmail.com>
2026-01-02 14:56:17 +0800

    feat(Plugins.Ping): add initial implementation of Ping plugin with configuration
    实现了第一个插件 ping & pong

---
+++ src/plugins/WoofBot.Plugins.Ping/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using WoofBot.Sdk.Interfaces;
+using WoofBot.Sdk.Models;
+
+namespace WoofBot.Plugins.Ping;
+
+public record PingPluginConfig
+{
+    public List<string> Admins { get; init; } = [];
+}
+
+public class PingPlugin : PluginBase<PingPluginConfig>
+{
+    public PingPlugin() : base("Ping", "1.0", "A simple ping plugin") {}
+
+    protected override async Task HandleEventAsync(Event evt, IAdapter adapter)
+    {
+        if (evt is MessageEvent msgEvt
+            && msgEvt.Target.Type == TargetType.Group
+            && Config.Admins.Contains(msgEvt.SenderId)
+            && msgEvt.Messages is [Text("ping")])
+        {
+            await adapter.SendMessageAsync(msgEvt.Target, [new Text("pong")]);
+        }
+    }
+}
