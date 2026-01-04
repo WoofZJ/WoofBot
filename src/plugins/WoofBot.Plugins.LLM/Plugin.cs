2c02ed4c4263511b206a774be947d1c3c1951c8f
WoofZJ <zjwoof@gmail.com>
2026-01-04 16:29:59 +0800

    feat(Plugins.LLM): enhance LLM plugin with response handling and wake word support
    实现了工具调用、图片输入，完成了基本的会话管理

--- src/plugins/WoofBot.Plugins.LLM/Plugin.cs
+++ src/plugins/WoofBot.Plugins.LLM/Plugin.cs
@@ -5,6 +5,10 @@ using System.ClientModel;
 using OpenAI.Chat;
 using Microsoft.Agents.AI;
 using System.Text;
+using System.ComponentModel;
+using Microsoft.Extensions.AI;
+using System.Text.Json;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
 
 namespace WoofBot.Plugins.LLM;
 
@@ -15,6 +19,18 @@ public record LLMPluginConfig
     public List<string> EnabledGroups { get; init; } = [];
     public string InstructionsFilePath { get; init; } = string.Empty;
     public string ModelName { get; init; } = string.Empty;
+    public List<string> WakeWords { get; init; } = [];
+}
+
+[Description("Response format that you should follow.")]
+public record LLMResponse
+{
+    [Description("Whether a response is needed.")]
+    public bool NeedResponse { get; init; } = false;
+    [Description("The text content to be sent in response.")]
+    public string ChatText { get; init; } = string.Empty;
+    [Description("Whether to end the current session.")]
+    public bool EndSession { get; init; } = false;
 }
 
 public class LLMPlugin : PluginBase<LLMPluginConfig>
@@ -22,6 +38,7 @@ public class LLMPlugin : PluginBase<LLMPluginConfig>
     public LLMPlugin() : base("LLM", "1.0", "A simple LLM plugin") {}
 
     private ChatClientAgent? _agent;
+    private Dictionary<string, AgentThread> _agentThreads = [];
 
     public override void Initialize()
     {
@@ -42,36 +59,87 @@ public class LLMPlugin : PluginBase<LLMPluginConfig>
         _agent = client
             .GetChatClient(Config.ModelName)
             .CreateAIAgent(
-                instructions: instructions
+                new ChatClientAgentOptions()
+                {
+                    Name = "WoofBot LLM Agent",
+                    ChatOptions = new()
+                    {
+                        Instructions = instructions,
+                        ResponseFormat = Microsoft.Extensions.AI.ChatResponseFormat.ForJsonSchema<LLMResponse>(),
+                        Tools = [AIFunctionFactory.Create(GetCurrentDateTime)]
+                    }
+                }
             );
     }
 
     protected override async Task HandleEventAsync(Event evt, IAdapter adapter)
     {
-        if (evt is MessageEvent msgEvt
-            && msgEvt.Target.Type == TargetType.Group
-            && Config.EnabledGroups.Contains(msgEvt.Target.Id)
-            && msgEvt.Messages.Contains(new At(adapter.SelfId))
-            && _agent is not null)
+        if (evt is not MessageEvent || _agent is null) return;
+        var msgEvt = (MessageEvent)evt;
+        if (msgEvt.Target.Type is not TargetType.Group
+            || !Config.EnabledGroups.Contains(msgEvt.Target.Id))
+            return;
+        if (msgEvt.Messages.Contains(new At(adapter.SelfId))
+            || msgEvt.Messages.Any(m => m is Text text && Config.WakeWords.Any(ww => text.Content.Contains(ww))))
         {
-            StringBuilder sb = new();
+            if (!_agentThreads.ContainsKey(msgEvt.Target.Id))
+            {
+                _agentThreads[msgEvt.Target.Id] = _agent.GetNewThread();
+            }
+        }
+        if (_agentThreads.TryGetValue(msgEvt.Target.Id, out AgentThread? thread))
+        {
+            StringBuilder sb = new($"<{msgEvt.Timestamp}> User({msgEvt.SenderId}): ");
+            List<string> images = [];
             foreach (var msg in msgEvt.Messages)
             {
-                sb.Append(msg switch
+                switch (msg)
                 {
-                    Text text => text.Content,
-                    At at => at.Target == adapter.SelfId ? "@(法仆塔)" : $"@({at.Target})",
-                    _ => string.Empty
-                });
+                    case Text text:
+                        sb.Append(text.Content);
+                        break;
+                    case At at:
+                        sb.Append(at.Target == adapter.SelfId ? "@(我)" : $"@({at.Target})");
+                        break;
+                    case ImageRecv img:
+                        sb.Append($"[图片,filename={img.File}]");
+                        if (img.FileSize <= 4 * 1024 * 1024)
+                        {
+                            // var imgBytes = await GetImageFromUrl(img.Url);
+                            // images.Add(imgBytes);
+                            images.Add(img.Url);
+                        }
+                        break;
+                }
             }
-            var userMessage = sb.ToString();
-            Console.WriteLine("User message: " + userMessage);
-            var response = await _agent.RunAsync(userMessage);
-
-            Console.WriteLine(response);
-            await adapter.SendMessageAsync(msgEvt.Target, [new Text(response.Text)]);
-
+            List<AIContent> contents = [new TextContent(sb.ToString())];
+            foreach (var imgUrl in images)
+            {
+                contents.Add(new UriContent(imgUrl, "image/jpeg"));
+            }
+            ChatMessage userMessage = new (ChatRole.User, contents);
+            Console.WriteLine(userMessage);
+            var response = await _agent.RunAsync<LLMResponse>(userMessage, thread);
+            var llmResponse = response.Deserialize<LLMResponse>(JsonSerializerOptions.Web);
+            Console.WriteLine(llmResponse);
+            if (llmResponse.NeedResponse)
+            {
+                await adapter.SendMessageAsync(msgEvt.Target, [new Text(llmResponse.ChatText)]);
+            }
+            if (llmResponse.EndSession)
+            {
+                // write thread to file
+                var json = thread.Serialize(JsonSerializerOptions.Web);
+                File.WriteAllText($"llm_thread_{msgEvt.Target.Id}_{DateTimeOffset.Now.ToUnixTimeSeconds()}.json", json.ToString());
+                _agentThreads.Remove(msgEvt.Target.Id);
+            }
         }
+    }
 
+    [Description("Get current Utc DateTime.")]
+    private static DateTime GetCurrentDateTime()
+    {
+        Console.WriteLine("GetCurrentDateTime called.");
+        return DateTime.UtcNow;
     }
 }
